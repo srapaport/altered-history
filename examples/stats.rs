54c717bc630d0340c38ff90b363accb69fa4c3d0
Solal <solal.rapaport@telecom-paris.fr>
2024-11-03 18:08:54 +0100

    first final draft

--- examples/stats.rs
+++ examples/stats.rs
@@ -1,16 +1,8 @@
-use anyhow::{ensure, Result};
 use altered_history::env;
-use once_cell::sync::Lazy;
 use swh_graph::graph::*;
 use swh_graph::NodeType;
-use std::collections::{HashMap, HashSet, VecDeque};
-use std::fs;
-use std::fs::File;
-use std::os::unix::fs::FileExt;
-use std::path::Path;
-use std::ffi::OsString;
-use std::io::{prelude::*, BufReader};
-use std::sync::mpsc::channel;
+use std::collections::HashSet;
+//use std::simd::num;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::cell::Cell;
@@ -18,9 +10,6 @@ use std::thread;
 use std::time::Instant;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::path::PathBuf;
-use ar_row::arrow::array::RecordBatch;
-use chashmap::CHashMap;
-use regex::Regex;
 use rayon::ThreadPoolBuilder;
 use rayon::prelude::*;
 use rocksdb::{DB, Options};
@@ -46,31 +35,15 @@ static TOTAL_REV: AtomicUsize = AtomicUsize::new(0);
 fn main(){
     let opts = altered_history::env::Options::parse();
 
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
-            WriteLogger::new(LevelFilter::Info, Config::default(), std::fs::File::create("./examples/branches_filter_FULL.log").unwrap()),
-        ]
-    ).unwrap();
-
-    let basename: &str;
-    let db_path: String;
-    let number_of_origins: usize; 
-    let filename_res: &str;
+    let stat_file: &str;
 
     match opts.dataset.as_str(){
         "2021" => {
-            basename = env::BASENAME_2021;
-            db_path = "/infres/ir800/rapaport/phd-solal/dev/rust/datasets/2021".to_string();
-            number_of_origins = env::ORIGINS_2021; 
-            filename_res = "branches_filter_2021.csv";
+            stat_file = "./examples/stats_2021.log";
         },
         "2023" => todo!(),
         "FULL" => {
-            basename = env::BASENAME_FULL;
-            db_path = format!("{}/db_origin_visit",env::DATABASE_FULL);
-            number_of_origins = env::ORIGINS_FULL;
-            filename_res = "branches_filter_FULL.csv";
+            stat_file = "./examples/stats_FULL.log";
         },
         _ => {
             warn!("Usage: <2021, 2023, FULL>");
@@ -78,7 +51,46 @@ fn main(){
         },
     }
 
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            WriteLogger::new(LevelFilter::Info, Config::default(), std::fs::File::create(stat_file).unwrap()),
+        ]
+    ).unwrap();
 
+    let start = Instant::now();
+    count(&opts);
+    info!("Stats complete | time elapsed: {:.2?}", start.elapsed());
+    info!("Origins kept: {}", ORI_KEPT.load(Ordering::Relaxed));
+    info!("Origins rejected: {}", ORI_REJECTED.load(Ordering::Relaxed));
+    info!("Branches kept in origins kept: {}", BRANCHES_KEPT.load(Ordering::Relaxed));
+    info!("Branches rejected in origins kept: {}", BRANCHES_REJECTED.load(Ordering::Relaxed));
+    info!("Total revisions in branches kept: {}", TOTAL_REV.load(Ordering::Relaxed));
+    
+}
+
+fn count(
+    opts: &env::Options,
+)
+{
+    let basename: &str;
+    let number_of_origins: usize;
+
+    match opts.dataset.as_str(){
+        "2021" => {
+            basename = env::BASENAME_2021;
+            number_of_origins = env::ORIGINS_2021;
+        },
+        "2023" => todo!(),
+        "FULL" => {
+            basename = env::BASENAME_FULL;
+            number_of_origins = env::ORIGINS_FULL;
+        },
+        _ => {
+            warn!("Usage: <2021, 2023, FULL>");
+            unimplemented!()
+        },
+    }
     let graph = SwhUnidirectionalGraph::new(PathBuf::from(basename))
         .expect("Could not load graph")
         .init_properties()
@@ -89,85 +101,120 @@ fn main(){
         .load_labels()
         .expect("Could not load labels");
 
-    count(&graph);
-    
-    
+    let props = graph.properties();
+    let count_origins = AtomicUsize::new(0);
+    let workers = num_cpus::get()/3;
+    let pool = ThreadPoolBuilder::new().num_threads(workers).build().unwrap();
+    let bar = ProgressBar::new(number_of_origins as u64);
+    bar.set_style(ProgressStyle::with_template("{wide_bar} {pos} {percent_precise}% {elapsed_precise} {duration_precise} {eta}").unwrap());
+    //Look for origins
+    for node in 0..graph.num_nodes(){
+        if props.node_type(node) != NodeType::Origin{
+            continue;
+        }
+        pool.install(||{
+            rayon::scope(|thread| {
+                count_origins.fetch_add(1, Ordering::Relaxed);
+                //Look for snapshots
+                thread.spawn(|_|{
+                    let mut kept: bool = false;
+                    let mut branches_kept = 0;
+                    let mut branches_rejected = 0;
+                    let mut revs = HashSet::new();
+                    let thread_id = unsafe { gettid() };
+                    debug!("Checking Origin: {} | with pid: {:?}", node, thread_id);
+                    for (succ_ori, _) in graph.labeled_successors(node){
+                        if props.node_type(succ_ori) != NodeType::Snapshot{
+                            warn!("succ of origin is not a snapshot");
+                            continue;
+                        }
+                        //Look for branches
+                        for (succ_snap, labels) in graph.labeled_successors(succ_ori){
+                            if props.node_type(succ_snap) != NodeType::Revision && props.node_type(succ_snap) != NodeType::Release{
+                                warn!("succ of snap is not a revision nor a release");
+                                continue;
+                            }
+                            
+                            for label in labels{
+                                let branch_name: String;
+                                if let EdgeLabel::Branch(branch) = label{
+                                    branch_name = String::from_utf8_lossy(
+                                        &(graph.properties().label_name(branch.filename_id()))
+                                    ).to_string();
+                                    if !env::RE_BRANCH.is_match(&branch_name){
+                                        branches_rejected += 1;
+                                        continue;
+                                    }
+                                    kept = true;
+                                    branches_kept += 1;
+                                    count_revisions(succ_snap, &mut revs, &graph);
+                                }
+                                else{
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                    if kept{
+                        ORI_KEPT.fetch_add(1, Ordering::Relaxed);
+                        BRANCHES_KEPT.fetch_add(branches_kept, Ordering::Relaxed);
+                        BRANCHES_REJECTED.fetch_add(branches_rejected, Ordering::Relaxed);
+                        TOTAL_REV.fetch_add(revs.into_iter().count(), Ordering::Relaxed);
+                    }
+                    else{
+                        ORI_REJECTED.fetch_add(1, Ordering::Relaxed);
+                    }
+                    let curr_nb_origins = count_origins.load(Ordering::Relaxed) as u64;
+                    bar.set_position(curr_nb_origins);
+                });
+            });
+        });
+    }
+    bar.finish_and_clear();
 }
 
-fn count<G: SwhLabeledForwardGraph + SwhGraphWithProperties>(
+fn count_revisions<G: SwhLabeledForwardGraph + SwhGraphWithProperties>(
+    rev_rel: usize,
+    revs: &mut HashSet<usize>,
     graph: &G
 )
 where
     <G as SwhGraphWithProperties>::Maps: swh_graph::properties::Maps,
     <G as SwhGraphWithProperties>::LabelNames: swh_graph::properties::LabelNames,
 {
-
     let props = graph.properties();
-    //Look for origins
-    for node in 0..graph.num_nodes(){
-        if props.node_type(node) != NodeType::Origin{
-            continue;
-        }
-        //Look for snapshots
-        let mut kept: bool = false;
-        let mut branches_kept = 0;
-        let mut branches_rejected = 0;
-        let mut nb_rev = 0;
-        for (succ_ori, _) in graph.labeled_successors(node){
-            if props.node_type(succ_ori) != NodeType::Snapshot{
-                warn!("succ of origin is not a snapshot");
+    let mut rev_opt= None;
+    if props.node_type(rev_rel) == NodeType::Release{
+        for succ in graph.successors(rev_rel){
+            if props.node_type(succ) != NodeType::Release{
+                let x = props.node_type(succ).to_str();
+                warn!("succ of release is not a release {x}");
                 continue;
             }
-            //Look for branches
-            for (succ_snap, labels) in graph.labeled_successors(succ_ori){
-                if props.node_type(succ_snap) != NodeType::Revision && props.node_type(succ_snap) != NodeType::Release{
-                    warn!("succ of snap is not a revision nor a release");
-                    continue;
-                }
-                
-                for label in labels{
-                    let branch_name: String;
-                    if let EdgeLabel::Branch(branch) = label{
-                        branch_name = String::from_utf8_lossy(
-                            &(graph.properties().label_name(branch.filename_id()))
-                        ).to_string();
-                        if !env::RE_BRANCH.is_match(&branch_name){
-                            branches_rejected += 1;
-                            continue;
-                        }
-                        kept = true;
-                        branches_kept += 1;
-                        todo!("Amount of commits in the branch"); 
-                        //nb_rev += count_revisions(succ_snap, graph);
-                    }
-                    else{
-                        continue;
-                    }
-                }
-            }
-        }
-        if kept{
-            ORI_KEPT.fetch_add(1, Ordering::Relaxed);
-            BRANCHES_KEPT.fetch_add(branches_kept, Ordering::Relaxed);
-            BRANCHES_REJECTED.fetch_add(branches_rejected, Ordering::Relaxed);
-            TOTAL_REV.fetch_add(nb_rev, Ordering::Relaxed);
-        }
-        else{
-            ORI_REJECTED.fetch_add(1, Ordering::Relaxed);
+            rev_opt = Some(succ);
         }
     }
-}
-
-fn count_revisions<G: SwhLabeledForwardGraph + SwhGraphWithProperties>(
-    rev_rel: usize,
-    graph: &G
-) -> usize
-where
-    <G as SwhGraphWithProperties>::Maps: swh_graph::properties::Maps,
-    <G as SwhGraphWithProperties>::LabelNames: swh_graph::properties::LabelNames,
-{
-    let mut nb_rev = 0;
-    //if rel -> rev else keep rev
-    //while succ type rev, nb_rev += 1
-    nb_rev
+    else{
+        rev_opt = Some(rev_rel);
+    }
+    if let Some(rev) = rev_opt{
+        let mut nodes_to_visit = Vec::new();
+        nodes_to_visit.push(rev);
+        let mut visited: HashSet<usize> = HashSet::new();
+        while let Some(node) = nodes_to_visit.pop(){
+            //debug!("size nodes_to_visit: {}", nodes_to_visit.len());
+            if visited.contains(&node){
+                continue;
+            }
+            visited.insert(node);
+            let successors = graph.successors(node);
+            for succ in successors{
+                if (props.node_type(succ) != NodeType::Revision) && (props.node_type(succ) != NodeType::Release){
+                    continue;
+                }
+                nodes_to_visit.push(succ);
+                revs.insert(node);
+            }
+        }
+    }
 }
