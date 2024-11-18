2013a6e5ccc8571d87c157af4f096e4d1f950a64
Solal <solal.rapaport@telecom-paris.fr>
2024-11-18 16:55:37 +0100

    opts already put for test purpuses

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,12 @@ use std::time::Instant;
 fn main() {
     //let opts = altered_history::env::Options::parse();
     let opts = altered_history::env::Options {
-        orc_dir: String::from(""),
-        output_dir: String::from(""),
+        orc_dir: String::from("/infres/ir800/rapaport/datasets/2021-03-23-popular-3k-python-orc/origin_visit_status"),
+        output_dir: String::from("./results/db_2021"),
         //graph: String::from("path/to/graph"),
-        graph: String::from("/infres/ir800/rapaport/datasets/2021-03-23-popular-3k-python-graph/graph"),
+        graph: String::from(
+            "/infres/ir800/rapaport/datasets/2021-03-23-popular-3k-python-graph/graph",
+        ),
         //results: String::from("/infres/ir800/rapaport/results/FULL_2024_08"),
         results: String::from("./results"),
         //expected_origins: 226_726_529,
@@ -25,8 +27,10 @@ fn main() {
     Logger::with(LevelFilter::Info)
         .log_to_file(
             flexi_logger::FileSpec::default()
-                .directory("/infres/ir800/rapaport/results/FULL_new/logs")
-                .basename("FULL-all")
+                // .directory("/infres/ir800/rapaport/results/FULL_new/logs")
+                // .basename("FULL-all")
+                .directory("./results/logs")
+                .basename("2021")
                 .suffix("log"),
         )
         .rotate(
@@ -38,10 +42,12 @@ fn main() {
         .start()
         .unwrap();
 
-    //Step 1
+    // Step 1
     println!("START");
     let start = Instant::now();
+    // Retrieve all snapshots per origin with their timestamps (only for teaser datasets)
     altered_history::visit_timestamps::retrieve_visit_timestamps(&opts);
+
     if let Some(cp) = altered_history::load_checkpoint(&opts) {
         altered_history::main_all_database_mpsc_with_cp(&opts, cp);
     } else {
