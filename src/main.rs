30de6a953d22d902d1d1348a6c4c02cc3ee0f3e0
Solal <solal.rapaport@telecom-paris.fr>
2024-11-26 12:06:12 +0100

    path for swh1

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let opts = altered_history::env::Options {
         orc_dir: String::from("/infres/ir800/rapaport/datasets/2021-03-23-popular-3k-python-orc/origin_visit_status"),
         output_dir: String::from("./results/db_2021"),
-        //graph: String::from("path/to/graph"),
+        //graph: String::from("/poolswh/softwareheritage/graph/2024-08-23/compressed/graph"),
         graph: String::from(
             "/infres/ir800/rapaport/datasets/2021-03-23-popular-3k-python-graph/graph",
         ),
